fd741d4f5822f868c2c5cea1e98edaa0881ac85c
JesusVG97 <jesusvg97cs@gmail.com>
2025-10-02 17:56:57 +0200

    Funcion 5 Filtros

--- app/src/main/java/Hello.kt
+++ app/src/main/java/Hello.kt
@@ -1,45 +1,38 @@
-import java.util.*    // required import
-
-fun randomDay() : String {
-    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
-        "Friday", "Saturday", "Sunday")
-    return week[Random().nextInt(week.size)]
-}
-
-fun fishFood (day : String) : String {
-    return when (day) {
-        "Monday" -> "flakes"
-        "Wednesday" -> "redworms"
-        "Thursday" -> "granules"
-        "Friday" -> "mosquitoes"
-        "Sunday" -> "plankton"
-        else -> "nothing"
+fun main() {
+    val decorations = listOf("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
+    // Paso 1: filtro simple
+    println(decorations.filter { it[0] == 'p' })
+    // Paso 2: comparación eager  vs lazy (
+    val eager = decorations.filter { it[0] == 'p' }
+    println("eager: $eager")
+    val filtered = decorations.asSequence().filter { it[0] == 'p' }
+    println("filtered: $filtered")
+    val newList = filtered.toList()
+    println("new list: $newList")
+    // Paso 3: map en secuencia
+    val lazyMap = decorations.asSequence().map {
+        println("access: $it")
+        it
     }
-}
-
-fun feedTheFish() {
-    val day = randomDay()
-    val food = fishFood(day)
-    println ("Today is $day and the fish eat $food")
-    println("Change water: ${shouldChangeWater(day)}")
-}
-fun swim(speed: String = "fast") {
-    println("swimming $speed")
-}
-fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
-    return when {
-        isTooHot(temperature) -> true
-        isDirty(dirty) -> true
-        isSunday(day) -> true
-        else  -> false
-    }
-}
-fun isTooHot(temperature: Int) = temperature > 30
-
-fun isDirty(dirty: Int) = dirty > 30
-
-fun isSunday(day: String) = day == "Sunday"
-
-fun main(args: Array<String>) {
-    feedTheFish()
+    println("lazy: $lazyMap")
+    println("-----")
+    println("first: ${lazyMap.first()}")
+    println("-----")
+    println("all: ${lazyMap.toList()}")
+    // Paso 4: combinación de filter + map (evaluación diferida)
+    val lazyMap2 = decorations.asSequence()
+        .filter { it[0] == 'p' }
+        .map {
+            println("access: $it")
+            it
+        }
+    println("-----")
+    println("filtered: ${lazyMap2.toList()}")
+    // Paso 5: uso de flatten()
+    val mysports = listOf("basketball", "fishing", "running")
+    val myplayers = listOf("LeBron James", "Ernest Hemingway", "Usain Bolt")
+    val mycities = listOf("Los Angeles", "Chicago", "Jamaica")
+    val mylist = listOf(mysports, myplayers, mycities)     // lista de listas
+    println("-----")
+    println("Flat: ${mylist.flatten()}")
 }
