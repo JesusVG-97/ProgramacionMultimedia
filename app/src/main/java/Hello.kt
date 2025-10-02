ea362f6cd011eb3a440e77d1d1b8dc872a2f5a07
JesusVG97 <jesusvg97cs@gmail.com>
2025-10-02 17:42:05 +0200

    3 more about function

--- app/src/main/java/Hello.kt
+++ app/src/main/java/Hello.kt
@@ -1,5 +1,28 @@
-fun main() {
-    val temperature = 10
-    val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK" }."
-    println(message)
+import java.util.*    // required import
+
+fun randomDay() : String {
+    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+        "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(week.size)]
+}
+
+fun fishFood (day : String) : String {
+    return when (day) {
+        "Monday" -> "flakes"
+        "Wednesday" -> "redworms"
+        "Thursday" -> "granules"
+        "Friday" -> "mosquitoes"
+        "Sunday" -> "plankton"
+        else -> "nothing"
+    }
+}
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+    println ("Today is $day and the fish eat $food")
+}
+
+fun main(args: Array<String>) {
+    feedTheFish()
 }
