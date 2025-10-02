136a3b81e50e926be47480770cfb7519e293aba5
JesusVG97 <jesusvg97cs@gmail.com>
2025-10-02 17:40:49 +0200

    Function 2

--- app/src/main/java/Hello.kt
+++ app/src/main/java/Hello.kt
@@ -1,3 +1,5 @@
-fun main(args: Array<String>) {
-    println("Hello, ${args[0]}")
+fun main() {
+    val temperature = 10
+    val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK" }."
+    println(message)
 }
