bf0f8f4456cef92f8c741f0e18106547936c8cdb
JesusVG97 <jesusvg97cs@gmail.com>
2025-10-04 10:59:03 +0200

    5. Learn about subclasses and inheritance

--- app/src/main/kotlin/example/myapp/Main.kt
+++ app/src/main/kotlin/example/myapp/Main.kt
@@ -1,10 +1,13 @@
 package example.myapp
+
 fun buildAquarium() {
-    val aquarium6 = Aquarium(numberOfFish = 29)
-    aquarium6.printSize()
-    aquarium6.volume = 70
-    aquarium6.printSize()
+    val myAquarium = Aquarium(width = 25, length = 25, height = 40)
+    myAquarium.printSize()
+
+    val myTower = TowerTank(diameter = 25, height = 40)
+    myTower.printSize()
 }
+
 fun main() {
     buildAquarium()
 }
