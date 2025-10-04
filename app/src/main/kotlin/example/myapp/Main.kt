38b110183985301c289f4e6688867615f9fe7aef
JesusVG97 <jesusvg97cs@gmail.com>
2025-10-04 11:46:47 +0200

    1. Create a Companion Object

--- app/src/main/kotlin/example/myapp/Main.kt
+++ app/src/main/kotlin/example/myapp/Main.kt
@@ -1,9 +1,9 @@
 package example.myapp
 
-import example.myapp.decor.Color
+/*import example.myapp.decor.Color
 import example.myapp.decor.Direction
 import example.myapp.decor.makeDecorations
-
+*/
 fun makeFish() {
     val shark = Shark()
     val pleco = Plecostomus()
@@ -13,12 +13,13 @@ fun makeFish() {
     pleco.eat()
 }
 fun main () {
-    //makeFish()
-    //makeDecorations()
+    /*makeFish()
+    makeDecorations()
     println(Color.RED)
     println(Color.BLUE)
     println(Color.GREEN)
     println(Direction.EAST.name)
     println(Direction.EAST.ordinal)
     println(Direction.EAST.degrees)
+    */
 }
